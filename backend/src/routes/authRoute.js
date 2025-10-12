f7ffd48b83fc5737ff4f8d6d1760c471da9af426
maswangskuy04 <reyhanmarlizal@gmail.com>
2025-10-12 15:50:32 +0700

    Update authcontroller

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { verifyToken } = require('../middlewares/authMiddleware')
-const { requestOtp, verifyOtp } = require('../controllers/authController')
+const { requestOtp, verifyOtp, logout } = require('../controllers/authController')
 
 router.post('/request-otp', requestOtp)
 router.post('/verify-otp', verifyOtp)
+router.post('/logout', logout)
 
 module.exports = router
